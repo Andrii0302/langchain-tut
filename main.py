4a6e7429eac7c57598f520d12c7d16aa66eb9e0a
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2026-06-10 12:41:02 +0300

    add simple chain flow

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 
 def main():
-    print("Hello from langchain-learn!")
+    information = """То́мас А́лва Е́дісон (англ. Thomas Alva Edison; 11 лютого 1847 Майлен, Огайо, США — 18 жовтня 1931, Вест-Орандж, Нью-Джерсі, США)  — американський науковець і винахідник, автор винаходів, на які видано 1093 патенти США та 1239 патентів інших країн[10]. Більшість своїх винаходів зробив у лабораторії в Менло-Парк у штаті Нью-Джерсі, протягом 1876—1887 років, включаючи створення лампи розжарення у 1879 році. Створив систему розподілу електроенергії споживачам, мікрофон для телефону, диктофон і фонограф. Він запропонував використовувати на початку телефонної розмови слово «алло». """
+    summary_template = """Given the given information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them"""
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+    llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-2.5-flash")
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 
 if __name__ == "__main__":
